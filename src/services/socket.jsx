39f8fa0fd484df43065c578a1fe5697d59dbc679
Nitesh Singh <onenitesh7@gmail.com>
2026-03-03 01:10:27 +0530

    Removed localhost, using VITE_BACKEND_URL

--- src/services/socket.jsx
+++ src/services/socket.jsx
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // Ensure this is 5001 to match your server
-const SOCKET_URL = "http://localhost:3001"; 
+const SOCKET_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false, 
